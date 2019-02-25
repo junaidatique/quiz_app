e6f72511c5f705ff4a303212084a58c6828155ea
Junaid Atique <junaidatique@gmail.com>
2019-02-25 15:42:47 +0500

    commit with login form

--- Gemfile
+++ Gemfile
@@ -62,3 +62,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'slim-rails'
+gem 'bootstrap', '~> 4.0.0'
+gem 'jquery-rails', '~> 4.3.1'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create({email: 'junaid.atique@telenor.com.pk', password: 'test1234'});
